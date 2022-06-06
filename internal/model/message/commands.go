4822e1dac39f1a46ff3e84c2b386a9203faef604
vilkov <alexander.vilkov@gridnine.com>
2022-06-06 10:27:31 +0300

    Добавлена обработка команд

---
+++ internal/model/message/commands.go
@@ -0,0 +1,9 @@
+package message
+
+const (
+	START           string = "/start"
+	GET_DYNAMIC     string = "/getdynamic"
+	ADD_NEW_PRODUCT string = "/addproduct"
+	GET_ALL_URLS    string = "/getmyurl"
+	DELETE_URL      string = "/deleteurl"
+)
--- internal/model/message/messageHandler.go
+++ internal/model/message/messageHandler.go
@@ -1,10 +1,27 @@
 package message
 
 import (
-	"PriceMonitoringClient/internal/priceMonitoringConnector"
-	"strconv"
+	"strings"
 )
 
 func HandleMessage(testMessage string, userID int) string {
-	return priceMonitoringConnector.SaveUrlForMonitoring("www.lolkek.ru", strconv.Itoa(userID))
+
+	if strings.Compare(strings.ToLower(testMessage), START) == 0 {
+		return "выбрана команда старт"
+	}
+	if strings.Compare(strings.ToLower(testMessage), GET_DYNAMIC) == 0 {
+		return "выбрана команда Показать динамику"
+	}
+	if strings.Compare(strings.ToLower(testMessage), ADD_NEW_PRODUCT) == 0 {
+		return "выбрана команда  Добавить продукт"
+	}
+	if strings.Compare(strings.ToLower(testMessage), GET_ALL_URLS) == 0 {
+		return "выбрана команда  Показать все url"
+	}
+
+	if strings.Compare(strings.ToLower(testMessage), DELETE_URL) == 0 {
+		return "выбрана команда  удалить url"
+	}
+	return "Не известная команда"
+	//return priceMonitoringConnector.SaveUrlForMonitoring("www.lolkek.ru", strconv.Itoa(userID))
 }
